0ae914d5c31b6ce3317838d7585ebd156ce746c8
Naviek <none>
2019-05-02 17:16:08 +0900

    added level select

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var csv = new Array();
+let csv = new Array();
+let getEle, searchstr, level;
 
 function load() {
-	csv = CSVtoArray('./data/lv17.csv');
+	getEle = GETtoDict(location.search.substring(1));
+	searchstr = decodeURI(getEle["searchstr"]).toLowerCase();
+	level = getEle["level"];
+	csv = CSVtoArray("./data/lv" + level + ".csv");
 }
 
 function CSVtoArray(url) {
@@ -28,27 +32,52 @@ function CSVtoArray(url) {
 	return arr2;
 }
 
-function search(searchValue) {
-	let s = document.getElementById(searchValue).value.toLowerCase();
+function GETtoDict(str) {
+	let rel = str.split('&');
+	let getEle = {};
+	for (let r of rel) {
+		e = r.split('=');
+		getEle[e[0]] = e[1];
+	}
+	return getEle;
+}
+
+function search() {
 
 	let cnt = 0;
 	ans = [];
 
 	for (let line of csv) {
 		for (let e of line) {
-			if (e.toLowerCase().indexOf(s) !== -1) {
+			if (e.toLowerCase().indexOf(searchstr) !== -1) {
 				cnt++;
 				ans.push([line[1], e]);
 			}
 		}
 	}
 
-	if (s === "" || s === null || cnt === 0) {
-		alert('not found');
+	if (searchstr === "" || searchstr === null || cnt === 0) {
+		return 'not found';
 	}
 	else {
-		for (let e of ans) {
-			alert(e[0] + ' ' + e[1]);
+		return ans;
+	}
+}
+
+function initResult() {
+	load();
+	let res = search();
+	let parent_object = document.getElementById("songs");
+	if (res === "not found") {
+		let list_element = document.createElement("p");
+		list_element.innerHTML = "not found";
+		parent_object.appendChild(list_element);
+	}
+	else {
+		for (let e of res) {
+			let list_element = document.createElement("li");
+			list_element.innerHTML = e[0] + ' ' + e[1];
+			parent_object.appendChild(list_element);
 		}
 	}
 }
